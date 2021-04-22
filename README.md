998713abb7b39917aa398fe8b4e5e07d0e181d47
wangyh <wangyhzzzz@gmail.com>
2021-04-22 14:41:12 +0800

    README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+## 使用方法
+将腾讯视频vid信息传入获得m3u8文件列表
+
+以下演示使用`ts-node`运行本程序：
+
+`ts-node src/app.ts e0024f5mhf8`
+
+想转成mp4的话，可以将m3u8列表地址传入ffmpeg(需要额外安装)，例如：
+
+`ffmpeg -i http://xxx.x.m3u8 -c copy -bsf:a aac_adtstoasc output.mp4`
+
+
+
+## vid获取
+ vid获取可以通过视频链接 (地址后面的`i0024rxw2f4`就是vid)
+
+https://v.qq.com/x/cover/jvhuaf93hh858fs/i0024rxw2f4.html
+
+
+
+## 仅供学习交流使用，请勿使用于其他用途
+
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import {Tencent} from "./Tencent";
 
 async function main() {
-    let episode = await Tencent.instance.getUrl("e0024f5mhf8");
+    const args = process.argv.slice(2)
+    if (args.length < 1) return console.log("输入视频vid")
+    let episode = await Tencent.instance.getUrl(args[0]);
     console.log(episode.url);
     console.log(`ffmpeg -i '${episode.url}' -c copy -bsf:a aac_adtstoasc output.mp4`);
 }
